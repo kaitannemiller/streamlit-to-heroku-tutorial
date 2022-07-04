ea00434135953ea7fe667181196be8534fe67070
Kaitlin Miller <kam5678@msn.com>
2022-07-04 16:59:58 -0700

    fixing

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -1,10 +1,12 @@
 
 import streamlit as st
+import gspread
 import pandas as pd
-from collections.abc import Iterable
-from collections.abc import MutableMapping
-import mo_parsing
-import gsheetsdb
+
+gc = gspread.service_account(filename='./cred.json')
+sheet1 = gc.open_by_url('https://docs.google.com/spreadsheets/d/1iudlYSDTvHLjEa0q04ebYmVnb8cV1xD7qJMUBj0rxFs/edit?usp=sharing')
+sh = sheet1.get_worksheet(0)
+df_gsheet = pd.DataFrame(data=sh.get_all_records())
 
 
 st.write("My First Streamlit Web App")
@@ -13,8 +15,8 @@ df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 
 st.title("Connect to Google Sheets")
-gsheet_url = "https://docs.google.com/spreadsheets/d/1iudlYSDTvHLjEa0q04ebYmVnb8cV1xD7qJMUBj0rxFs/edit?usp=sharing"
-conn = gs.connect()
-rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-df_gsheet = pd.DataFrame(rows)
+#gsheet_url = "https://docs.google.com/spreadsheets/d/1iudlYSDTvHLjEa0q04ebYmVnb8cV1xD7qJMUBj0rxFs/edit?usp=sharing"
+#conn = gs.connect()
+#rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
+#df_gsheet = pd.DataFrame(rows)
 st.write(df_gsheet)
