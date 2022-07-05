a251e7572e1b083d1855749e474ecbd8c0e4c18f
Kaitlin Miller <kam5678@msn.com>
2022-07-04 21:04:40 -0700

    layout3

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -12,14 +12,37 @@ df_gsheet = pd.DataFrame(data=sh.get_all_records())
 st.set_page_config(page_title="BACHELOR BETS", page_icon="./rose.ico", layout='wide')
 padding = 0
 st.markdown(""" <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    .reportview-container .main .block-container{{
-        padding-top: {padding}rem;
-        padding-right: {padding}rem;
-        padding-left: {padding}rem;
-        padding-bottom: {padding}rem;
-    }} </style> """, unsafe_allow_html=True)
+#MainMenu {visibility: hidden;}
+header {visibility: hidden;}
+footer {visibility: hidden;}
+.css-18e3th9 {
+    background-color: grey;
+    padding: 0rem 0rem 0rem 0rem;
+    flex: 1 1 0%;
+}
+#root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) {
+    flex: 1 1 0%;
+    height: 100%;
+}
+div.stButton > button:first-child {
+    background-color: #00cc00;
+    color:white;
+    font-size:20px;
+    height:3em;
+    width:100%;
+}
+#root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(7) {
+    position: absolute;
+    bottom: 0px;
+}
+.st-bd {border-style: none;}
+.css-qri22k {
+    font-size: 0px;
+    padding: 0rem 0rem 0rem 0rem;
+}
+</style> """, unsafe_allow_html=True)
+
+
 
 st.write("My First Streamlit Web App")
 
@@ -37,6 +60,8 @@ def my_widget(key):
     return st.button("Click me " + key)
 
 # And within an expander
-my_expander = st.expander("Expand", expanded=True)
+my_expander = st.expander("Expand")
 with my_expander:
     clicked = my_widget("second")
+
+clicked2 = my_widget("third")
