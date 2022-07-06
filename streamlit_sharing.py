4aab6e8a07b28e0cee6e5b2f7de159ec43c69e73
Kaitlin Miller <kam5678@msn.com>
2022-07-06 00:45:07 -0700

    update tues

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -2,137 +2,212 @@
 import streamlit as st
 import gspread
 import pandas as pd
+import streamlit_authenticator as stauth
+import time
+import base64
+import yaml
+
+st.set_page_config(page_title="BACHELOR BETS", page_icon="./rose.ico", layout='wide')
+
+
+bet_i = 0
+def create_page(name):
+    global bet_i
+
+    st.markdown(""" <style>
+    #MainMenu {visibility: hidden;}
+    .css-k0sv6k {
+        height: 5rem;
+        background-color: transparent;
+        border-style: outset;
+    }
+    .css-fk4es0.e8zbici1 {visibility: hidden;}
+    .header-custom {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        color: black;
+        font-size: 50px;
+        height: 5rem;
+        padding: 0rem 0rem 0rem 0rem;
+        flex: 1 1 0%;
+        background-color: #F7F7F7;
+        text-align: center;
+        font-family: Brush Script MT;
+    }
+    .e1tzin5v3 {
+        font-size: 0px;
+    }
+    div.stButton > button:first-child {
+        background-color: #00cc00;
+        color:white;
+        font-size:20px;
+        min-height:3em;
+        width:100%;
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(4) > div {
+        height: 4.5rem;
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(4) > div > div {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(4) > div > div > p {
+        text-align: center;
+        font-size: 28px;
+        color: #333333;
+        margin: 0px 0px 0px;
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(6) {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        color: black;
+        font-size: 14px;
+        height: 5rem;
+        padding: 0rem 0rem 0rem 0rem;
+        display: flex;
+        flex-direction: row;
+        flex: 1 1 0%;
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(6) > div {
+        display: flex;
+        flex-direction: row;
+        flex: 1 1 0%;
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(6) > div > div {
+        display: flex;
+        flex-direction: row;
+        flex: 1 1 0%;
+        width: 33%;
+        padding: 2px 2px 2px 2px;
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(6) > div > div > div > button {
+        background-color: #D90429;
+        color: black;
+        font-size:20px;
+        min-height:0em;
+        height:100%;
+        width: 100%;
+        padding: 2px 2px 2px 2px;
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(6) > div > div > div > button:hover {
+        background-color: #D90429;
+        color: white;
+        font-size:20px;
+        min-height:0em;
+        height:100%;
+        width: 100%;
+        padding: 2px 2px 2px 2px;
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(6) > div > div > div > button:active {
+        background-color: #D90429;
+        color: black;
+        font-size:20px;
+        min-height:0em;
+        height:100%;
+        width: 100%;
+    }
+    footer {
+        position: static;
+    }
+    .css-18e3th9 {
+        padding: 5rem 0rem 0rem 0rem;
+    }
+    .e1tzin5v0 {
+        gap: 0rem;
+    }
+    .ehezqtx6 {
+        font-size: 8px;
+        text-align: right;
+    }
+    .streamlit-expander {
+        background-color: #F7F7F7;
+        border-style: inset;
+    }
+    .st-ae {
+        font-size: 20px;
+    }
+    .css-qri22k {
+        font-size: 0px;
+        padding: 5rem 0rem 0rem 0rem;
+    }
+    </style> """, unsafe_allow_html=True)
+
+
+    file_ = open("rose.ico", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+
+    header =f"""
+        <div class="header-custom">
+            <img src="data:image/gif;base64,{data_url}" alt="rose" height="40px" width="40px">
+            {name}
+            <img src="data:image/gif;base64,{data_url}" alt="rose" height="40px" width="40px">
+        </div>
+    """
+    st.markdown(header, unsafe_allow_html=True)
+
+
+    st.write("Week One")
+
+    # And within an expander
+    bet_titles = ["Gabby's First Impression Rose","Rachel's First Impression Rose","Week One Rose Ceremony","Number of Guys Sent Home","Most Popular Buzz Word"]
+
+
+    def bet_container_go():
+        global bet_i
+        global bet_container
+        bet_container = st.empty()
+        with bet_container.container():
+            i = 0
+            while i < len(bet_titles):
+                my_expander = st.expander(bet_titles[i])
+                with my_expander:
+                    clicked = st.button("Click me", key='button'+str(bet_i))
+                i = i + 1
+                bet_i = bet_i + 1
+
+    bet_container_go()
+
+    button_container = st.empty()
+    with button_container.container():
+        contestants_button = st.button("Contestants", key='contestants_button')
+        bets_button = st.button("Bets", key='bets_button')
+        standings_button = st.button("Standings", key='standings_button')
+    if contestants_button or standings_button:
+        bet_container.empty()
+    if bets_button:
+        bet_container.empty()
+        bet_container_go()
+
+
 
 gc = gspread.service_account(filename='./cred.json')
 sheet1 = gc.open_by_url('https://docs.google.com/spreadsheets/d/1iudlYSDTvHLjEa0q04ebYmVnb8cV1xD7qJMUBj0rxFs/edit?usp=sharing')
 sh = sheet1.get_worksheet(0)
 df_gsheet = pd.DataFrame(data=sh.get_all_records())
 
+with open('./cred.yaml') as file:
+    config = yaml.safe_load(file)
 
-st.set_page_config(page_title="BACHELOR BETS", page_icon="./rose.ico", layout='wide')
-padding = 0
-st.markdown(""" <style>
-#MainMenu {visibility: hidden;}
-.css-k0sv6k {
-    height: 5rem;
-    background-color: transparent;
-    border-style: outset;
-}
-.css-fk4es0.e8zbici1 {visibility: hidden;}
-.header-custom {
-    position: fixed;
-    top: 0;
-    width: 100%;
-    color: black;
-    font-size: 50px;
-    height: 5rem;
-    padding: 0rem 0rem 0rem 0rem;
-    flex: 1 1 0%;
-    background-color: #F7F7F7;
-    text-align: center;
-    font-family: Brush Script MT;
-}
-.e1tzin5v3 {
-    width: 100%;
-}
-.footer-custom {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    color: black;
-    font-size: 14px;
-    height: 5rem;
-    padding: 0rem 0rem 0rem 0rem;
-    flex: 1 1 0%;
-}
-footer {
-    position: static;
-}
-.css-18e3th9 {
-    padding: 5rem 0rem 0rem 0rem;
-}
-.e1tzin5v0 {
-    gap: 0rem;
-}
-div.stButton > button:first-child {
-    background-color: #00cc00;
-    color:white;
-    font-size:20px;
-    min-height:3em;
-    width:100%;
-}
-.footer-custom > div > div.stButton > button:first-child {
-    background-color: #D90429;
-    color: black;
-    font-size:20px;
-    min-height:0em;
-    height:100%;
-    width:100%;
-}
-.footer-custom > div > div.stButton > button:first-child:hover {
-    background-color: #D90429;
-    color: white;
-    font-size:20px;
-    min-height:0em;
-    height:100%;
-    width:100%;
-}
-.footer-custom > div > div.stButton > button:first-child:active {
-    background-color: #D90429;
-    color: black;
-    font-size:20px;
-    min-height:0em;
-    height:100%;
-    width:100%;
-}
-.st-bd {
-    background-color: #D2D3D1;
-    border-style: inset;
-}
-.css-qri22k {
-    font-size: 0px;
-    padding: 5rem 0rem 0rem 0rem;
-}
-</style> """, unsafe_allow_html=True)
+for user in config['credentials']['usernames'].keys():
+    config['credentials']['usernames'][user]['password'] = stauth.Hasher([config['credentials']['usernames'][user]['password']]).generate()[0]
 
-footer = """
-    <div class="footer-custom">
-        <div class="element-container css-bo0d43 e1tzin5v3" style="height: 100%; display: flex; flex-direction: row; flex: 1 1 0%;">
-            <div class="row-widget stButton" style="left: 0; width: 33.3%; padding: 3px 3px 3px 3px;"><button kind="primary" class="css-1cpxqw2 edgvbvh9 footer_button1">Footer1</button></div>
-            <div class="row-widget stButton" style="width: 33.3%; padding: 3px 3px 3px 3px;"><button kind="primary" class="css-1cpxqw2 edgvbvh9 footer_button2">Footer2</button></div>
-            <div class="row-widget stButton" style="right: 0;  width: 33.3%; padding: 3px 3px 3px 3px;"><button kind="primary" class="css-1cpxqw2 edgvbvh9 footer_button3">Footer3</button></div>
-        </div>
-    </div>
-"""
-st.markdown(footer, unsafe_allow_html=True)
+authenticator = stauth.Authenticate(config['credentials'],'bachelor_bets','harrison',cookie_expiry_days=30)
+name, authentication_status, username = authenticator.login('Login','main')
+time.sleep(0.5)
 
-import base64
-file_ = open("rose.ico", "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
-
-name = "Claire"
-header =f"""
-    <div class="header-custom">
-        <img src="data:image/gif;base64,{data_url}" alt="rose" height="40px" width="40px">
-        {name}
-        <img src="data:image/gif;base64,{data_url}" alt="rose" height="40px" width="40px">
-    </div>
-"""
-st.markdown(header, unsafe_allow_html=True)
-
-
-#gsheet_url = "https://docs.google.com/spreadsheets/d/1iudlYSDTvHLjEa0q04ebYmVnb8cV1xD7qJMUBj0rxFs/edit?usp=sharing"
-#conn = gs.connect()
-#rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-#df_gsheet = pd.DataFrame(rows)
-#st.write(df_gsheet)
-
-# And within an expander
-i = 0
-while i < 7:
-    my_expander = st.expander("Expand")
-    with my_expander:
-        clicked = st.button("Click me", key='button'+str(i))
-    i = i + 1
+if st.session_state['authentication_status']:
+    #authenticator.logout('Logout', 'main')
+    if st.session_state["name"] != None:
+        name = st.session_state["name"].split(' ')[0]
+        create_page(name)
+elif st.session_state['authentication_status'] == False:
+    st.error('Username/password is incorrect')
+elif st.session_state['authentication_status'] == None:
+    st.warning('Please enter your username and password.')
